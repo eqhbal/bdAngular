7c6bf329f9c0a6d0b925a61ea3934721072f862f
eghbol <eqhbal@gmail.com>
2021-05-18 01:33:27 +0200

    lab 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Contact } from './models/contact';
+import {FormGroup, FormControl, Validator, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,10 @@ import { Contact } from './models/contact';
 export class AppComponent {
   title = 'bdAngular';
 
+  addContactForm = new FormGroup({
+    email: new FormControl('', Validators.pattern('^.+@.+\.nl$'))
+  });
+
   name: string;
   constructor() {
     this.name = 'world!';
@@ -17,5 +22,16 @@ export class AppComponent {
     { firstName: 'Sam', surname: 'Smith', email: 'sam.smith@music.com' },
     { firstName: 'Frank', surname: 'Muscles', email: 'frank@muscles.com' },
     { firstName: 'Eddy', surname: 'Valentino', email: 'eddy@valfam.co.uk' }
-  ];
+    ];
+    newContact = {} as Contact;
+
+    addContact(): void {
+      console.log('Submitted value: ', this.addContactForm.value);
+    }
+
+
+    deleteContact(contact: Contact): void {
+      let index = this.contacts.indexOf(contact);
+      this.contacts.splice(index, 1);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import {ContactNamePipe} from './pipes/contact-name.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactNamePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+
